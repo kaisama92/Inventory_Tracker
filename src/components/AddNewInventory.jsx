e25811693050f8b21ee4a4946ffcc1e3b6662732
Kai Clausen <kaiclausen123@gmail.com>
2023-04-21 11:54:22 -0700

    create form to add coffee

--- src/components/AddNewInventory.jsx
+++ src/components/AddNewInventory.jsx
@@ -3,7 +3,29 @@ import React from "react";
 function NewTicketForm(props){
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewInventoryItemSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='name of coffee' />
+        <input
+          type='text'
+          name='roast'
+          placeholder='Roast Type i.e. "Dark"' />
+        <input
+          type="text"
+          name="origin"
+          placeholder="Origin i.e. Cambodia" />
+        <input 
+          type="number"
+          name="quantity"
+          placeholder="Quantity of burlap bags" />
+        <input 
+          type="text"
+          name="price"
+          placeholder="$1.99" />
+        <button type='submit'>Add!</button>
+      </form>
     </React.Fragment>
   );
 }
