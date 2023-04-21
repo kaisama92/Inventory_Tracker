46530c7fd6d6f2282ec3bd025cfc3feedabae986
Kai Clausen <kaiclausen123@gmail.com>
2023-04-21 11:21:25 -0700

    update MerchControl to InventoryControl

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "./Header";
-import MerchControl from "./MerchControl";
+import InventoryControl from "./InventoryControl";
 
 function App(){
   return ( 
     <React.Fragment>
       <Header />
-      <MerchControl /> 
+      <InventoryControl /> 
     </React.Fragment> 
   );
 }
