cb4e1c104f85ecdd4bd9548590ae73de2297f355
Kai Clausen <kaiclausen123@gmail.com>
2023-04-21 10:59:54 -0700

    remove references to merch

--- src/components/MerchList.jsx
+++ src/components/MerchList.jsx
@@ -1,44 +1,29 @@
 import React from 'react';
-import Merch from './Merch';
+import Inventory from './Merch';
 import MerchControl from './MerchControl';
 import PropTypes from 'prop-types';
 
-// export function MerchItem(props, id){
-//   const merchItem = props.mainMerchList[id]
-//   return (
-//     <React.Fragment>
-//       <hr/> 
-//         <Merch 
-//           name = {merchItem.name}
-//           description = {merchItem.description}
-//           quantity = {merchItem.quantity}
-//           price = {merchItem.price}
-//         />
-//     </React.Fragment>
-//   );
-// }
-
-export function MerchItem(props){
+export function InventoryItem(props){
   console.log("got past the button");
   console.log(props);
-  const { visiblemerch } = props;
-  console.log(props.visiblemerch.name);
-  console.log("^ This should be the merch");
+  const { visibleItem } = props;
+  console.log(props.visibleItem.name);
+  console.log("^ This should be the Inventory Item");
   return (
     <React.Fragment>
       <hr/> 
         <h1>Item Detail</h1>
-        <h2><em>{visiblemerch.name}</em></h2>
+        <h2><em>{visibleItem.name}</em></h2>
         <h3>Description</h3>
-        <p>{visiblemerch.description}</p>
+        <p>{visibleItem.description}</p>
         <h3>Quantity in Stock</h3>
-        <p>{visiblemerch.quantity}</p>
-        <h3>{visiblemerch.price}</h3>
+        <p>{visibleItem.quantity}</p>
+        <h3>{visibleItem.price}</h3>
     </React.Fragment>
   );
 }
 
-export function MerchList(props){
+export function InventoryList(props){
   function handlePassingOnlyOneItem(event) {
     console.log("" + event.target.value + " <-- Value on button ");
     props.onClickButton(event.target.value)
@@ -46,27 +31,27 @@ export function MerchList(props){
   return (
     <React.Fragment>
       <hr/>
-      {props.mainMerchList.map((merch) => (
+      {props.mainInventoryList.map((item) => (
         <div>
-          <Merch 
-            name = {merch.name}
-            description = {merch.description}
-            quantity = {merch.quantity}
-            price = {merch.price}
-            key = {merch.id}
+          <Inventory 
+            name = {item.name}
+            description = {item.description}
+            quantity = {item.quantity}
+            price = {item.price}
+            key = {item.id}
           />
-          <button value={merch.id} onClick={handlePassingOnlyOneItem}>Details</button>
+          <button value={item.id} onClick={handlePassingOnlyOneItem}>Details</button>
         </div>
       ))};
     </React.Fragment>
   );
 }
 
-MerchList.propTypes = {
+InventoryList.propTypes = {
   onClickButton: PropTypes.func,
-  mainMerchList: PropTypes.array
+  mainInventoryList: PropTypes.array
 };
 
-MerchItem.propTypes = {
+InventoryItem.propTypes = {
   item: PropTypes.object
 };
